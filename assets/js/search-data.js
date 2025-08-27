a90e9b3f747985569ea3d9ea178190531463d873
jingxiao-ma <32466413+jingxiao-ma@users.noreply.github.com>
2025-08-27 02:14:32 +0000

    Deploying to gh-pages from @ jingxiao-ma/jingxiao-ma.github.io@54cbbca5b90453af5350ad27c9b750f9b01c7118 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "I have been the teaching assistant for the following courses.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
